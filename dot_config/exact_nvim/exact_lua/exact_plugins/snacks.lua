d9e2d12d7f9bd031c617db75a2461ff2727d0814
Eddie McConkie <eddie.mcconkie@alphamountain.ai>
2026-06-03 10:45:55 -0600

    Update .config/nvim/lua/plugins/snacks.lua Update .config/nvim/stylua.toml

--- dot_config/exact_nvim/exact_lua/exact_plugins/snacks.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/snacks.lua
@@ -1,31 +1,18 @@
 return {
-  {
-    "snacks.nvim",
-    opts = {
-		--  root = {
-		-- detector = false,
-		--  },
-      scroll = {
-        enabled = false
-        -- animate = {
-        --   duration = 0,
-        -- },
-        -- -- animate = {
-        -- --   duration = {
-        -- --     step = 10,
-        -- --     total = 100,
-        -- --   },
-        -- --   easing = "linear",
-        -- -- },
-        -- animate_repeat = {
-        --   delay = 0,
-        --   duration = {
-        --     step = 0,
-        --     total = 0,
-        --   },
-        --   easing = "linear",
-        -- },
-      },
-    },
-  },
+	{
+		"snacks.nvim",
+		opts = {
+			picker = {
+				sources = {
+					explorer = {
+						hidden = true,
+						ignored = true,
+					},
+				},
+			},
+			scroll = {
+				enabled = false,
+			},
+		},
+	},
 }
