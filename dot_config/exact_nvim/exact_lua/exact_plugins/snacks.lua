effcec9dea5a55c82f3faa51de83d7d4455da317
Eddie McConkie <eddie.mcconkie@alphamountain.ai>
2026-08-26 23:45:35 -0600

    Update .config/nvim/lua/plugins/snacks.lua Update .config/nvim/lazy-lock.json

--- dot_config/exact_nvim/exact_lua/exact_plugins/snacks.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/snacks.lua
@@ -3,10 +3,12 @@ return {
 		"snacks.nvim",
 		opts = {
 			picker = {
+				exclude = { "*.uid", "*.import", "addons/**/*" },
 				sources = {
 					explorer = {
 						hidden = true,
 						ignored = true,
+						exclude = { "*.uid", "*.import" },
 					},
 				},
 			},
